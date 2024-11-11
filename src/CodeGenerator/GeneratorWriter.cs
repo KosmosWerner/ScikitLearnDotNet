cc8d65274bc748552118360ae88cf9f62de5dbaf
Kosmos Werner <kosmoswerner@gmail.com>
2024-11-10 22:32:31 -0500

    GeneratorWriter: Updated

--- src/CodeGenerator/GeneratorWriter.cs
+++ src/CodeGenerator/GeneratorWriter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,15 +17,16 @@ namespace CodeGenerator
         ];
 
         private readonly IWriter writer;
-        private readonly string currentNamespace;
-        private readonly string currentClass;
+        private readonly string current_namespace;
+        private readonly string current_static_class;
         private readonly int total_lines;
         private bool printedHeaders = false;
-        public GeneratorWriter(IWriter writer, string currentNamespace, int total_lines)
+        public GeneratorWriter(IWriter writer, string current_namespace, int total_lines)
         {
             this.writer = writer;
-            this.currentNamespace = currentNamespace;
-            this.currentClass = currentNamespace.Split(".")[^1];
+            this.current_namespace = current_namespace;
+            this.current_static_class = current_namespace.Split(".")[^1];
+            if (names.Contains(current_static_class)) current_static_class = "@" + current_static_class;
             this.total_lines = total_lines;
         }
 
@@ -45,14 +47,98 @@ namespace CodeGenerator
             writer.WriteLine();
         }
 
-        private void PrinClassStart()
+        HashSet<string> names = new(
+        [
+            "abstract",
+            "as",
+            "base",
+            "bool",
+            "break",
+            "byte",
+            "case",
+            "catch",
+            "char",
+            "checked",
+            "class",
+            "const",
+            "continue",
+            "decimal",
+            "default",
+            "delegate",
+            "do",
+            "double",
+            "else",
+            "enum",
+            "",
+            "event",
+            "explicit",
+            "extern",
+            "false",
+            "finally",
+            "fixed",
+            "float",
+            "for",
+            "foreach",
+            "goto",
+            "if",
+            "implicit",
+            "in",
+            "int",
+            "interface",
+            "internal",
+            "is",
+            "lock",
+            "long",
+            "",
+            "namespace",
+            "new",
+            "null",
+            "object",
+            "operator",
+            "out",
+            "override",
+            "params",
+            "private",
+            "protected",
+            "public",
+            "readonly",
+            "ref",
+            "return",
+            "sbyte",
+            "sealed",
+            "short",
+            "sizeof",
+            "stackalloc",
+            "",
+            "static",
+            "string",
+            "struct",
+            "switch",
+            "this",
+            "throw",
+            "true",
+            "try",
+            "typeof",
+            "uint",
+            "ulong",
+            "unchecked",
+            "unsafe",
+            "ushort",
+            "using",
+            "virtual",
+            "void",
+            "volatile",
+            "while",
+        ]);
+
+        private void PrintClassStart()
         {
             writer.WriteLine("public static partial class sklearn");
             writer.WriteLine("{");
 
-            if (currentNamespace != currentClass)
+            if (current_namespace != current_static_class)
             {
-                writer.WriteLine($"\tpublic static class {currentClass}");
+                writer.WriteLine($"\tpublic static class {current_static_class}");
                 writer.WriteLine("\t{");
             }
         }
@@ -63,7 +149,7 @@ namespace CodeGenerator
             writer.WriteLine();
             writer.WriteLine("\t\tpublic static PyObject self => _lazy_self.Value;");
             writer.WriteLine();
-            writer.WriteLine($"\t\tstatic {currentClass}() => ReInitializeLazySelf();");
+            writer.WriteLine($"\t\tstatic {current_static_class}() => ReInitializeLazySelf();");
             writer.WriteLine();
             writer.WriteLine("\t\tprivate static void ReInitializeLazySelf()");
             writer.WriteLine("\t\t{");
@@ -78,32 +164,181 @@ namespace CodeGenerator
             writer.WriteLine("\t\t{");
             writer.WriteLine("\t\t\tPythonEngine.AddShutdownHandler(ReInitializeLazySelf);");
             writer.WriteLine("\t\t\tPythonEngine.Initialize();");
-            writer.WriteLine($"\t\t\treturn Py.Import(\"{currentNamespace}\");");
+            writer.WriteLine($"\t\t\treturn Py.Import(\"{current_namespace}\");");
             writer.WriteLine("\t\t}");
             writer.WriteLine();
         }
 
-        public void PrintHeaderOneTime()
+        public void PrintStartStaticClass()
         {
             if (!printedHeaders)
             {
                 PrintUsings();
                 PrintNamespace();
-                PrinClassStart();
+                PrintClassStart();
                 PrintImportModules();
                 printedHeaders = true;
             }
         }
 
-        public void PrintClose(int current_line)
+        public void PrintEndStaticClass()
         {
-            if (current_line == total_lines)
+            if (current_namespace != current_static_class)
             {
-                if (currentNamespace != currentClass)
+                writer.WriteLine("\t}");
+            }
+            writer.WriteLine("}");
+        }
+
+        public void PrintStartClass(string class_name)
+        {
+            writer.WriteLine($"\t\tpublic class {class_name} : PythonObject");
+            writer.WriteLine("\t\t{");
+        }
+
+        public void PrintEndClass()
+        {
+            writer.WriteLine("\t\t}");
+            writer.WriteLine();
+        }
+
+
+        public void PrintParamsList(
+            Dictionary<string, string> param_args,
+            Dictionary<string, string> param_kw,
+            Dictionary<string, string> param_types)
+        {
+            int total = param_args.Count + param_kw.Count;
+            int i = 0;
+            foreach (var item in param_args)
+            {
+                i++;
+                if (param_types.TryGetValue(item.Key, out string? type))
                 {
-                    writer.WriteLine("\t}");
+                    if (string.IsNullOrEmpty(item.Value)) writer.Write($"{type} {item.Key}");
+                    else writer.Write($"{type} {item.Key} = {item.Value}");
+                    if (i != total) writer.Write(", ");
                 }
-                writer.WriteLine("}");
+            }
+            foreach (var item in param_kw)
+            {
+                i++;
+                if (param_types.TryGetValue(item.Key, out string? type))
+                {
+                    if (string.IsNullOrEmpty(item.Value)) writer.Write($"{type} {item.Key}");
+                    else writer.Write($"{type} {item.Key} = {item.Value}");
+                    if (i != total) writer.Write(", ");
+                }
+            }
+        }
+
+        public void PrintInvokableArguments(
+            Dictionary<string, string> param_args,
+            Dictionary<string, string> param_kw,
+            Dictionary<string, string> param_types)
+        {
+            if (param_args.Count > 0)
+            {
+                string args = string.Join(", ", param_args.Select(x => x.Key).ToArray());
+                writer.WriteLine($"\t\t\t\tPyTuple args = ToTuple(new object[] {{{args}}});");
+            }
+            else
+            {
+                writer.WriteLine("\t\t\t\tPyTuple args = new PyTuple();");
+            }
+
+            writer.WriteLine("\t\t\t\tPyDict pyDict = new PyDict();");
+
+            foreach (var item in param_kw)
+            {
+                var type = param_types[item.Key];
+                if (type.EndsWith('?'))
+                {
+                    writer.WriteLine($"\t\t\t\tif ({item.Key} != null)");
+                    writer.WriteLine("\t\t\t\t{");
+                    writer.WriteLine($"\t\t\t\t\tpyDict[\"{item.Key}\"] = ToPython({item.Key});");
+                    writer.WriteLine("\t\t\t\t}");
+                }
+                else
+                {
+                    writer.WriteLine($"\t\t\t\tpyDict[\"{item.Key}\"] = ToPython({item.Key});");
+                }
+            }
+        }
+
+
+        public void PrintConstructor(
+            string class_name,
+            Dictionary<string, string> ctor_args,
+            Dictionary<string, string> ctor_kw,
+            Dictionary<string, string> ctor_param_types)
+        {
+            writer.Write($"\t\t\tpublic {class_name}(");
+            PrintParamsList(ctor_args, ctor_kw, ctor_param_types);
+            writer.WriteLine(")");
+
+            writer.WriteLine("\t\t\t{");
+
+            PrintInvokableArguments(ctor_args, ctor_kw, ctor_param_types);
+            writer.WriteLine($"\t\t\t\tself = {current_static_class}.self.InvokeMethod(\"{class_name}\", args, pyDict);");
+
+
+            writer.WriteLine("\t\t\t}");
+            writer.WriteLine();
+        }
+
+        public void PrintMethods(
+            string method_name,
+            Dictionary<string, string> method_args,
+            Dictionary<string, string> method_kw,
+            Dictionary<string, string> method_types,
+            string return_type,
+            bool self = false)
+        {
+            writer.Write($"\t\t\tpublic {return_type} {method_name}(");
+            PrintParamsList(method_args, method_kw, method_types);
+            writer.WriteLine(")");
+
+            writer.WriteLine("\t\t\t{");
+            PrintInvokableArguments(method_args, method_kw, method_types);
+
+            if (self)
+            {
+                writer.WriteLine($"\t\t\t\tself.InvokeMethod(\"{method_name}\", args, pyDict);");
+                writer.WriteLine("\t\t\t\treturn this;");
+            }
+            else
+            {
+                writer.WriteLine($"\t\t\t\tPyObject result = self.InvokeMethod(\"{method_name}\", args, pyDict);");
+
+                if (return_type == "PyObject")
+                    writer.WriteLine($"\t\t\t\treturn result;");
+                else
+                    writer.WriteLine($"\t\t\t\treturn ToCsharp<{return_type}>(result);");
+            }
+
+            writer.WriteLine("\t\t\t}");
+            writer.WriteLine();
+        }
+
+        public void PrintAttributes(Dictionary<string, string[]> attributes)
+        {
+            foreach (var (key, types) in attributes)
+            {
+                string result = types.Length switch
+                {
+                    1 => types[0] == "PyObject"
+                        ? $"\t\t\tpublic PyObject {key} => self.GetAttr(\"{key}\");"
+                        : $"\t\t\tpublic {types[0]} {key} => ToCsharp<{types[0]}>(self.GetAttr(\"{key}\"));",
+
+                    2 when types.Contains("NDarray") && (types.Contains("bool") || types.Contains("int") || types.Contains("float"))
+                        => $"\t\t\tpublic NDarray {key} => ToCsharp<NDarray>(self.GetAttr(\"{key}\"));",
+
+                    _ => throw new ArgumentException($"PrintAttributes: Unable to convert {string.Join(", ", types)}")
+                };
+
+                writer.WriteLine(result);
+                writer.WriteLine();
             }
         }
     }

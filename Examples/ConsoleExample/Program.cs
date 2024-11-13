c93e4a75ce1e116c616b78c93375e3a35f6702bf
Kosmos Werner <kosmoswerner@gmail.com>
2024-11-13 02:35:48 -0500

    Console Example: Added

---
+++ Examples/ConsoleExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using Python.Runtime;
+using Python.Included;
+using Numpy;
+using ScikitLearn;
+
+namespace ConsoleExample
+{
+    internal class Program
+    {
+        private static async Task InitializeInstallerAsync()
+        {
+            Installer.InstallPath = Path.GetFullPath(".");
+
+            await Installer.SetupPython();
+            await Installer.TryInstallPip();
+            await Installer.PipInstallModule("numpy");
+            await Installer.PipInstallModule("scikit-learn");
+        }
+
+        static void Main(string[] args)
+        {
+            Task.Run(InitializeInstallerAsync).Wait();
+
+
+            var X = np.array(new int[,] {
+                { 1, 2 }, { 2, 2 }, { 2, 3 },
+                { 8, 7 }, { 8,8 }, { 25, 25 } });
+
+            var clustering = new sklearn.cluster.DBSCAN(eps: 3, min_samples: 2).fit(X);
+            Console.WriteLine(clustering.labels_);
+        }
+    }
+}

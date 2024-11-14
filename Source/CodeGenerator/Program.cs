04e997faf36338c38fa61e4acc5d9b9d1f3ce474
Kosmos Werner <kosmoswerner@gmail.com>
2024-11-14 02:59:23 -0500

    CodeGenerator: Removed Output

--- Source/CodeGenerator/Program.cs
+++ Source/CodeGenerator/Program.cs
@@ -9,7 +9,6 @@ internal class Program
         string currentPath = Path.GetFullPath(".");
         string destinationPath = Path.GetFullPath(Path.Combine(currentPath, "..", "..", "..", "..", "ScikitLearn"));
 
-        Console.WriteLine(destinationPath);
         var urls = await UriSearcher.Search("https://scikit-learn.org/stable/api/sklearn.html");
 
         await Generator.GenerateFrom(urls);
